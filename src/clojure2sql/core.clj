8f6045119444895a3fcf402bc720728b28137701
alynch <alexlynch1@hotmail.co.uk>
2017-10-28 16:48:36 +0100

    added more tests covering common functionality

--- src/clojure2sql/core.clj
+++ src/clojure2sql/core.clj
@@ -13,27 +13,66 @@
       (str "\"" obj "\"")
     :else
     (str obj)))
-(defn)
-(defn query-> [& forms]
-  (clojure.string/join "\n" forms))
+
+(defn query-> [query]
+  (clojure.pprint/pprint query)
+  (let [sql-object { :header [] :mid [] :footer [] }
+        ])
+  query)
+
+;
 
 (defn select [& fields]
-  (str "SELECT " (clojure.string/join (map #(output-str %) fields))))
+  { :type "select" :fields fields})
 
-(defn from [table]
-  (str "FROM " (output-str table)))
+(defn from [table & clauses]
+  { :type "from" :table table :clauses clauses})
+
+(defn update [table & clauses]
+  { :type "update" :clauses clauses})
+
+(defn delete-from [table & clauses]
+  { :type "delete" :clauses clauses})
+
+;
 
 (defn where [condition]
-  (str "WHERE " condition))
+  { :type "where" :cond condition})
 
-(defn update [table]
-  (str "UPDATE " (output-str table)))
+(defn group-by [col & clauses]
+  { :type "group-by" :col col :clauses clauses})
 
-(defn set [& options]
-  (str "SET " (clojure.string/join ", " options)))
+;
 
-(defn delete-from [table]
-  (str "DELETE FROM " (output-str table)))
+(defn set [& fields]
+  { :type "set" :fields fields})
+
+(defn count [field]
+  { :type "count" :field field})
 
 (defn = [lhs rhs]
-  (str (output-str lhs) " = " (output-str rhs)))
+  { :type "=" :lhs lhs :rhs rhs})
+
+(defn > [lhs rhs]
+  { :type ">" :lhs lhs :rhs rhs})
+
+; joins
+
+(defn inner-join [table on & clauses]
+  { :type "inner-join" :table table :on on :clauses clauses})
+
+(defn left-join [table on & clauses]
+  { :type "left-join" :table table :on on :clauses clauses})
+
+(defn right-join [table on & clauses]
+  { :type "right-join" :table table :on on :clauses clauses})
+
+;
+
+(defn on= [lhs rhs]
+  { :type "on=" :lhs lhs :rhs rhs})
+
+;
+
+(defn nested [query]
+  { :type "nested" :query query})
--- test/clojure2sql/core_test.clj
+++ test/clojure2sql/core_test.clj
@@ -5,11 +5,10 @@
 (deftest select-test
   (let [sql (query->
               (from :users
-                (select :name)))
+                (select :firstname :surname)))
         expected
 "SELECT name
 FROM users"]
-    (println "print:\n" sql)
     (is (= sql expected))))
 
 (deftest where-test
@@ -21,7 +20,6 @@ FROM users"]
 "SELECT name
 FROM users
 WHERE name = 'Smith'"]
-    (println "print:\n" sql)
     (is (= sql expected))))
 
 
@@ -34,31 +32,98 @@ WHERE name = 'Smith'"]
 "UPDATE users
 SET firstname = 'John', surname = 'Smith'
 WHERE id = 5"]
-    (println "print:\n" sql)
     (is (= sql expected))))
 
 (deftest delete-test
     (let [sql (query->
-                (delete-from :users)
-                (where (= :id 5)))
+                (delete-from :users
+                 (where (= :id 5))))
         expected
 "DELETE FROM users
 WHERE id = 5"]
-    (println "print:\n" sql)
     (is (= sql expected))))
 
 
-(deftest join-test
+(deftest inner-join-test
     (let [sql (query->
                 (from :users
                  (select :name)
                  (where (= :name "Smith"))
-                 (middle-join :roles (on= :userId :userId)
+                 (inner-join :roles (on= :userId :userId)
                   (select :role))))
             expected
 "SELECT name, role
 FROM users
 INNER JOIN roles on users.userId = roles.userId
 WHERE name = 'Smith'"]
-        (println "print:\n" sql)
         (is (= sql expected))))
+
+(deftest left-outer-join-test
+         (let [sql (query->
+                     (from :users
+                           (select :name)
+                           (where (= :name "Smith"))
+                           (left-join :roles (on= :userId :userId)
+                                       (select :role))))
+               expected
+               "SELECT name, role
+               FROM users
+               LEFT OUTER JOIN roles on users.userId = roles.userId
+               WHERE name = 'Smith'"]
+           (is (= sql expected))))
+
+(deftest right-outer-join-test
+         (let [sql (query->
+                     (from :users
+                           (select :name)
+                           (where (= :name "Smith"))
+                           (right-join :roles (on= :userId :userId)
+                                       (select :role))))
+               expected
+               "SELECT name, role
+               FROM users
+               RIGHT OUTER JOIN roles on users.userId = roles.userId
+               WHERE name = 'Smith'"]
+           (is (= sql expected))))
+
+(deftest group-by-test
+         (let [sql (query->
+                     (from :users
+                       (group-by :country)
+                       (select (count :name) :country)))
+               expected
+               "SELECT COUNT(name), country
+               FROM users
+               GROUP BY country"]
+           (is (= sql expected))))
+
+(deftest group-by-having-test
+         (let [sql (query->
+                     (from :users
+                           (group-by :country
+                             (where (> (count :name) 5)))
+                           (select (count :name) :country)))
+               expected
+               "SELECT COUNT(name), country
+               FROM users
+               GROUP BY country
+               HAVING surname = 'Smith'"]
+           (is (= sql expected))))
+
+(deftest nested-query-test
+         (let [get-russia-population (query->
+                       (from :countries
+                             (select :population)
+                             (where (= :name "Russia"))))
+               sql (query->
+                     (from :countries
+                      (select :name)
+                      (where (> :population (nested get-russia-population)))))
+               expected
+"SELECT name
+FROM countries
+WHERE population > (
+  SELECT population
+  FROM countries
+  WHERE name = 'Russia')"]
+           (is (= sql expected))))
